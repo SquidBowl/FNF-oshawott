1423f0f2f05dd2788c87707e41f081a1f8b76df9
Heckat <marie.vicco@gmail.com>
2023-11-27 00:02:38 +0100

    I hope I didn't fuck everything up

--- assets/stages/grassBG-sunset.lua
+++ assets/stages/grassBG-sunset.lua
@@ -1,1 +1,4 @@
-function onCreate()makeLuaSprite('stage', 'backgrounds/forest/grassBG-sunset', -250, -230);addLuaSprite('stage', false);end
+function onCreate()
+	makeLuaSprite('stage', 'backgrounds/forest/grassBG-sunset', -140, 360);
+	addLuaSprite('stage', false);
+end
---
+++ example_mods/scripts/CustomGo.lua
@@ -0,0 +1,34 @@
+function onCreate()
+	if songName == "Watering Shiny" or songName == "Shellshock shiny" or songName == "Ocean Aggrivation Shiny" then
+		makeAnimatedLuaSprite('Go Shine','UI/Go Shine',550,1060)addAnimationByPrefix('Go Shine','dance','Go Shine',40,true);
+	end
+	if songName == "OshawUgh Shiny" then
+		makeAnimatedLuaSprite('Go Shine','UI/Go Shine',130,425)addAnimationByPrefix('Go Shine','dance','Go Shine',40,true);
+	end
+	if songName == "Wott Shiny" then
+		makeAnimatedLuaSprite('Go Shine','UI/Go Shine',200,460)addAnimationByPrefix('Go Shine','dance','Go Shine',40,true);
+	end
+	if songName == "Figure Faceoff Shiny" or songName == "Figure Faceoff Shiny OLD" then
+		makeAnimatedLuaSprite('Go Shine','UI/Go Shine',-1200,300)addAnimationByPrefix('Go Shine','dance','Go Shine',40,true);
+	end
+	objectPlayAnimation('Go Shine','dance',false);
+	setScrollFactor('Go Shine', 1, 1);
+		if songName == "Figure Faceoff Shiny" or songName == "Figure Faceoff Shiny OLD" then
+			setGraphicSize('Go Shine',600,600);
+		else
+			setGraphicSize('Go Shine',450,450);
+		end
+end
+function onCountdownTick(c)
+	if c == 3 then
+		if songName == "Asperita Shiny" or songName == "Littleroot Shiny" or songName == "Dance Shiny" or songName == "Drunken Opiate Shiny" then --songs without pokemons
+			playSound('IntroGoShiny',1)
+		end
+		if songName == "Watering Shiny" or songName == "Shellshock shiny" or songName == "Ocean Aggrivation Shiny" or songName == "OshawUgh Shiny" or songName == "Wott Shiny" or songName == "Figure Faceoff Shiny" or songName == "Figure Faceoff Shiny OLD" then
+			addLuaSprite('Go Shine',true);
+			doTweenAlpha('ShineBye', 'Go Shine', 0,1.1,'linear');
+			playSound('IntroGoShiny',1)
+		end
+	end
+end
+
