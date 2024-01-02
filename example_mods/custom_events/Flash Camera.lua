3d8f43a36ab1e8bd467c355fdd2d0211ac1b73b7
NeonValvez <97431838+SquidBowl@users.noreply.github.com>
2024-01-02 17:19:07 -0500

    Revert "removed a shitload of lua scripts"
    This reverts commit 84d7f92e9eb8e6779516890ca369a1ce8c7da5ed.

--- example_mods/custom_events/Flash Camera.lua
+++ example_mods/custom_events/Flash Camera.lua
@@ -1,13 +1,5 @@
-function onEvent(n,v1,v2)
-	if n == 'Flash Camera' then
-	   makeLuaSprite('flash', '', 0, 0);
-        makeGraphic('flash',1280,720,'ffffff')
-	      addLuaSprite('flash', true);
-	      setLuaSpriteScrollFactor('flash',0,0)
-	      setProperty('flash.scale.x',2)
-	      setProperty('flash.scale.y',2)
-	      setProperty('flash.alpha',0)
-		setProperty('flash.alpha',1)
-		doTweenAlpha('flTw','flash',0,v1,'linear')
+function onEvent(name, val1, val2)
+	if name == 'Flash Camera' then
+		cameraFlash("game", 'FFFFFF', val2)
 	end
 end
