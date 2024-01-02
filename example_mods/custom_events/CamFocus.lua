84d7f92e9eb8e6779516890ca369a1ce8c7da5ed
NeonValvez <97431838+SquidBowl@users.noreply.github.com>
2024-01-02 17:10:15 -0500

    removed a shitload of lua scripts
    If you have any questions on how to replace or fix these lmk

--- example_mods/custom_events/CamFocus.lua
+++
@@ -1,6 +0,0 @@
-function onEvent(name, value1)
-if name == 'CamFocus' then
-cameraSetTarget(value1)
---debugPrint('Done: ', value1)
-end
-end
--- example_mods/custom_events/Spotlight.lua
+++
@@ -1,5 +0,0 @@
-function onEvent(name, value1, value2)
-	if name == "Spotlight" then
-		doTweenAlpha('spotlight', 'spotlight', value1, value2, 'linear');
-	end
-end
